f1d60f4189ae8022d722d063896f1390b47b17be
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2023-12-13 00:57:13 +0330

    add user registry

--- registry/registry.go
+++ registry/registry.go
@@ -1,1 +1,30 @@
 package registry
+
+import "cart-manager/domain/user"
+
+type ServiceRegistry interface {
+	mustImplementBaseRegistry()
+	RegisterUserService(string, user.UserService)
+	GetUserService() user.UserService
+}
+
+type serviceRegistry struct {
+	services ServiceMap
+}
+
+func mustImplementBaseRegistry() {}
+
+type ServiceMap struct {
+	UserServices user.UserService
+	// ProfileService ProfileService
+}
+
+func NewServiceRegistry() *serviceRegistry {
+	//create an empty service registry
+	sr := new(serviceRegistry)
+	return sr
+}
+
+func (sr *serviceRegistry) RegisterUserService(service user.UserService) {
+	sr.services.UserServices = service
+}
